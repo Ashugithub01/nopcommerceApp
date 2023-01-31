47c5562eff5be368e5a03a4dc9700ef2c10c5d74
ashish <verma101ashish@gmail.com>
2023-01-31 22:51:25 +0530

    5 commit

--- run.bat
+++ run.bat
@@ -1,19 +1,3 @@
 
 pytest -s -v -m "sanity" --html=./Reports/sanity_Regression.html TestCases/ --browser chrome
 
-rem pytest -s -v -m "sanity" --html=./Reports/sanity_Regression.html TestCases/ --browser chrome
-rem pytest -s -v -m "regression" --html=./Reports/sanity_Regression.html TestCases/ --browser chrome
-rem pytest -s -v -m "sanity or regression" --html=./Reports/sanity_Regression.html TestCases/ --browser chrome
-rem pytest -s -v -m "sanity and regression" --html=./Reports/sanity_Regression.html TestCases/ --browser chrome
-
-pytest -s -v -m "sanity" --html=./Reports/sanity_Regression.html TestCases/ --browser firefox
-
-rem pytest -s -v -m "regression" --html=./Reports/sanity_Regression.html TestCases/ --browser firefox
-rem pytest -s -v -m "sanity or regression" --html=./Reports/sanity_Regression.html TestCases/ --browser firefox
-rem pytest -s -v -m "sanity and regression" --html=./Reports/sanity_Regression.html TestCases/ --browser firefox
-
-pytest -s -v -m "sanity" --html=./Reports/sanity_Regression.html TestCases/ --browser edge
-
-rem pytest -s -v -m "regression" --html=./Reports/sanity_Regression.html TestCases/ --browser edge
-rem pytest -s -v -m "sanity or regression" --html=./Reports/sanity_Regression.html TestCases/ --browser edge
-rem pytest -s -v -m "sanity and regression" --html=./Reports/sanity_Regression.html TestCases/ --browser edge
